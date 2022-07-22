be50a06198fc394083debe23bc139517692dcc6c
KenStarry <starrycodes@gmail.com>
2022-07-23 00:50:12 +0300

    Pilot

--- Amplayer/app/src/main/java/com/example/amplayer/MainActivity.kt
+++ Amplayer/app/src/main/java/com/example/amplayer/MainActivity.kt
@@ -1,18 +1,11 @@
-package com.example.browserv1
+package com.example.amplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val webView: WebView = findViewById(R.id.webView)
-
-        webView.loadUrl("https://github.com")   // load url
-        webView.settings.javaScriptEnabled = true   //  enable javascript
     }
 }
